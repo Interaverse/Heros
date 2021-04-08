acfa612fe778bd14d19bef06279c014d4d633cc3
Interaverse <equinoxteam001@gmail.com>
2021-04-08 22:09:39 +1000

    create a feature component

---
+++ listOfHeros/src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input} from '@angular/core';
+import { Hero } from '../app.component';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  @Input() hero?: Hero;
+}
