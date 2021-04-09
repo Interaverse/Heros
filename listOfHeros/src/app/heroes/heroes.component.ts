2b663f5709650ccbb3dc657d163d99a588a33832
Interaverse <equinoxteam001@gmail.com>
2021-04-09 20:27:13 +1000

    Add Services

--- listOfHeros/src/app/heroes/heroes.component.ts
+++ listOfHeros/src/app/heroes/heroes.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../app.component';
-import { HEROES } from '../mock.heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes : Hero[] = [];
 
   hero: Hero = {
     id: 1,
@@ -17,11 +18,16 @@ export class HeroesComponent implements OnInit {
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
-  constructor() { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
   ngOnInit(): void {
+    this.getHeroes();
   }
 
 }
