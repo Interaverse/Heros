6b4c0b9b475aa21b6f2d69033f452cbc3c30c990
Interaverse <equinoxteam001@gmail.com>
2021-04-10 21:46:41 +1000

    ..

--- listOfHeros/src/app/hero.service.ts
+++ listOfHeros/src/app/hero.service.ts
@@ -48,5 +48,11 @@ export class HeroService {
     return of(result as T);
     }
   }
-
+  /** PUT: update the hero on the server */
+updateHero(hero: Hero): Observable<any> {
+  return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+    tap(_ => this.log(`updated hero id=${hero.id}`)),
+    catchError(this.handleError<any>('updateHero'))
+  );
+}
 }
