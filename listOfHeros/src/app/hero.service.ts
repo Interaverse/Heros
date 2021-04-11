a417f032fb65e9fb02066b205b64ccc1577b6098
Interaverse <equinoxteam001@gmail.com>
2021-04-11 12:24:37 +1000

    finished

--- listOfHeros/src/app/hero.service.ts
+++ listOfHeros/src/app/hero.service.ts
@@ -64,4 +64,23 @@ export class HeroService {
       catchError(this.handleError<Hero>('addHero'))
     );
   }
+/** DELETE: delete the hero from the server */
+deleteHero(id: number): Observable<Hero> {
+  const url = `${this.heroesUrl}/${id}`;
+
+  return this.http.delete<Hero>(url, this.httpOptions).pipe(
+    tap(_ => this.log(`deleted hero id=${id}`)),
+    catchError(this.handleError<Hero>('deleteHero'))
+  );
+}
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      return of([]);
+    }
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(x => x.length ? this.log(`found heroes matching "${term}"`) : this.log(`no heroes matching "${term}"`),
+        catchError(this.handleError<Hero[]>('searchHeroes', [])))
+    );
+
+  }
 }
